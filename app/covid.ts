97a2ac1c4b9cf998afcd82da0905cf89c832e562
hunt3ri <iain.hunter@outlook.com>
2020-07-11 12:55:13 +0100

    WIP

--- app/covid.ts
+++ app/covid.ts
@@ -4,9 +4,18 @@ import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import moment from 'moment';
 
-let test: string = "Bob";
-console.log("Hello TS ABI")
-let abi = test;
+type CovidStat = {
+    week: number
+    total: number
+}
+
+type CovidStats = {
+    totalDeaths: Array<CovidStat>
+};
+
+
+const week = document.getElementById('week');
+const weekLbl = document.getElementById('weekLbl');
 
 const map = new Map({
     target: 'map',
@@ -22,6 +31,60 @@ const map = new Map({
         minZoom: 5
     })
 });
+
+const getJSON = function (url: string, callback: any) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        let status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+
+function getTotalDeaths(weekNo: number) {
+    getJSON("https://raw.githubusercontent.com/hunt3ri/scot-covid-geo-coder/master/data/totalDeaths.json",
+        function (err: string, data: CovidStats) {
+            if (err !== null) {
+                alert('Something went wrong: ' + err);
+            } else {
+                //console.log("Week is: " + weekNo);
+                for (let element of data["totalDeaths"]) {
+                    if (element["week"] == weekNo) {
+                        //console.log("Total is " + element["total"])
+                        weekLbl!.innerHTML = "Week " + weekNo + ' - ' + getDate(weekNo) + ' - Total Deaths: ' + element["total"];
+
+                    }
+
+                }
+            }
+        });
+}
+
+// var weekHandler = function () {
+//     map.removeLayer(glbHeatmapLayer)
+//     map.removeLayer(glbPointsLayer)
+//     setHeatMapLayer(week.value)
+//     setPointsLayer(week.value)
+//     getTotalDeaths(week.value)
+// };
+
+
+
+//week.addEventListener('change', weekHandler);
+
+
+
+
+console.log("Hello TS ABI")
+let abi = test;
+
+
 
 
 export function getDate(weeksToAdd: number): string {
