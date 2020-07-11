a3bde445bd3a0ef66e757631b45ab0a6aad3adfb
hunt3ri <iain.hunter@outlook.com>
2020-07-11 12:23:44 +0100

    Add map

--- app/covid.ts
+++ app/covid.ts
@@ -1,8 +1,27 @@
+import {Map, View} from 'ol';
+import {fromLonLat} from 'ol/proj'
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
 import moment from 'moment';
 
 let test: string = "Bob";
 console.log("Hello TS ABI")
 let abi = test;
+
+const map = new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new OSM()
+        })
+    ],
+    view: new View({
+        center: fromLonLat([-3.828743, 56.758342]),
+        zoom: 7,
+        maxZoom: 9,
+        minZoom: 5
+    })
+});
 
 
 export function getDate(weeksToAdd: number): string {
