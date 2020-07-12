e2c3f8e90ae0d8f58e427ffa623094d507a2fe4b
hunt3ri <iain.hunter@outlook.com>
2020-07-12 12:45:50 +0100

    Add vue

--- app/covid.ts
+++ app/covid.ts
@@ -4,6 +4,7 @@ import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import moment from 'moment';
 import axios from 'axios';
+import Vue from 'vue'
 
 type CovidStat = {
     week: number
@@ -14,51 +15,40 @@ type CovidStats = {
     totalDeaths: CovidStat[]
 };
 
-const map = new Map({
-    target: 'map',
-    layers: [
-        new TileLayer({
-            source: new OSM()
+function setMap() {
+    return new Map({
+        target: 'map',
+        layers: [
+            new TileLayer({
+                source: new OSM()
+            })
+        ],
+        view: new View({
+            center: fromLonLat([-3.828743, 56.758342]),
+            zoom: 7,
+            maxZoom: 9,
+            minZoom: 5
         })
-    ],
-    view: new View({
-        center: fromLonLat([-3.828743, 56.758342]),
-        zoom: 7,
-        maxZoom: 9,
-        minZoom: 5
-    })
-});
+    });
+}
 
 function setTotalDeaths(weekNo: number): void {
     axios.get("https://raw.githubusercontent.com/hunt3ri/scot-covid-geo-coder/master/data/totalDeaths.json")
         .then(function (response) {
-            console.log(response)
+            console.log(weekNo)
             let covidStats: CovidStats = response.data;
+
             covidStats.totalDeaths.forEach(stat => {
-                    if (stat.week === weekNo) {
-                        weekLbl.innerHTML = "Week " + weekNo + ' - ' + getDate(weekNo) + ' - Total Deaths: ' + stat.total;
-                    }
-                })
+                if (stat.week === weekNo) {
+                    //weekLbl.innerHTML = "Week " + weekNo + ' - ' + getDate(weekNo) + ' - Total Deaths: ' + stat.total;
+                    app.message = "Week " + weekNo + ' - ' + getDate(weekNo) + ' - Total Deaths: ' + stat.total;
+                }
+            })
         })
         .catch(function (error) {
             alert('Error retrieving JSON file');
         });
 }
-
-const week: HTMLInputElement = document.getElementById('week') as HTMLInputElement;
-const weekLbl: HTMLLabelElement = document.getElementById('weekLbl') as HTMLLabelElement;
-
-
-var weekHandler = function () {
-    // map.removeLayer(glbHeatmapLayer)
-    // map.removeLayer(glbPointsLayer)
-    // setHeatMapLayer(week.value)
-    // setPointsLayer(week.value)
-    //getTotalDeaths(Number(week.value))
-    setTotalDeaths(Number(week.value))
-};
-
-week!.addEventListener('change', weekHandler);
 
 
 export function getDate(weeksToAdd: number): string {
@@ -67,4 +57,28 @@ export function getDate(weeksToAdd: number): string {
     return startDate.format("DD-MMM-YYYY")
 }
 
-setTotalDeaths(1)
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: "",
+        selectedWeek: "1"  // Set to 1 to ensure we reinitialise on reload
+    },
+    mounted() {
+        map: {
+            setMap();
+            setTotalDeaths(1);
+        }
+    },
+    methods: {
+        weekHandler(weekNoStr: string) {
+            let weekNo = Number(weekNoStr)
+            // map.removeLayer(glbHeatmapLayer)
+            // map.removeLayer(glbPointsLayer)
+            // setHeatMapLayer(week.value)
+            // setPointsLayer(week.value)
+            //getTotalDeaths(Number(week.value))
+            setTotalDeaths(weekNo)
+        }
+    }
+});
